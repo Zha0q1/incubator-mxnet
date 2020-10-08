57679393fd1fd862954d4868c120ddd18a45f0c6
Zhu <zhaoqzhu@3c22fbbb4e1a.ant.amazon.com>
2020-10-07 17:23:10 -0700

    d

--- src/operator/numpy/np_broadcast_reduce_op.h
+++ src/operator/numpy/np_broadcast_reduce_op.h
@@ -562,7 +562,7 @@ void NumpyArgMaxCompute(const nnvm::NodeAttrs& attrs,
 
     if (req[0] == kNullOp) return;
     Shape<NDim> rshape, rstride;
-    //diffuuu(out_data.shape_.get<NDim>(), in_data.shape_.get<NDim>(), &rshape, &rstride);
+    diffuuu(out_data.shape_.get<NDim>(), in_data.shape_.get<NDim>(), &rshape, &rstride);
     size_t N = out_data.shape_.Size(), M = rshape.Size();
     //seq_reduce_compute<mshadow_op::argmax, NDim, OType, DType, OType, op::mshadow_op::identity>(
     //  N, M, req[0] == kAddTo, in_data.dptr<DType>(), out_data.dptr<OType>(),
