138a7e33e673b60e6b3265f5421a9a49870ce8f6
Zhu <zhaoqzhu@3c22fbbb4e1a.ant.amazon.com>
2020-10-08 16:39:59 -0700

    a

--- src/operator/numpy/np_broadcast_reduce-inl.cuh
+++ src/operator/numpy/np_broadcast_reduce-inl.cuh
@@ -37,8 +37,8 @@ void NumpyArgMinMaxReduce(Stream<gpu> *s, const TBlob& in_data, const TBlob& out
   std::cout << "dududu" << std::endl;
   
 //   if (req == kNullOp) return;
-//  cudaStream_t stream = Stream<gpu>::GetStream(s);
-//  ReduceImplConfig config(out_date.shape_, in_data.shape_, nullptr, nullptr, sizeof(OType));
+  cudaStream_t stream = Stream<gpu>::GetStream(s);
+  ReduceImplConfig config(out_date.shape_, in_data.shape_, nullptr, nullptr, sizeof(OType));
 //   if (safe_acc) {
 //     MXNET_ACC_TYPE_SWITCH(mshadow::DataType<DType>::kFlag, DataType, AType, {
 //       typedef typename std::conditional<safe_acc, AType, DataType>::type AccType;
@@ -53,6 +53,57 @@ void NumpyArgMinMaxReduce(Stream<gpu> *s, const TBlob& in_data, const TBlob& out
 //   } else {
 //     ReduceImpl<Reducer, ndim, DType, DType, DType, OP>(stream, small, req, big, workspace, config);
 //   }
+
+/*
+  if (config.M == 1) {
+    reduce_kernel_M1<Reducer, ndim, AType, DType, OType, OP>
+    <<< config.kernel_1.gridDim, config.kernel_1.blockDim, 0, stream >>>(
+      config.N, false, big.dptr<DType>(), small.dptr<OType>(), big.shape_.get<ndim>(),
+      small.shape_.get<ndim>());
+    MSHADOW_CUDA_POST_KERNEL_CHECK(reduce_kernel_M1);
+  } else {
+    OType* small_dptr = reinterpret_cast<OType*>(in_data.dptr_)small.dptr<OType>();
+    bool addto = (req == kAddTo);
+    if (config.Mnext > 1) {
+      // small_dptr[] is N*Mnext*sizeof(DType) bytes
+      small_dptr = reinterpret_cast<OType*>(workspace.dptr_);
+      addto = false;
+      // Check that the workspace is contigiuous
+      CHECK_EQ(workspace.CheckContiguous(), true);
+      // Check that we have enough storage
+      CHECK_GE(workspace.size(0), config.workspace_size);
+    }
+
+    const int by = (config.kernel_1.do_transpose) ?
+      config.kernel_1.blockDim.x : config.kernel_1.blockDim.y;
+    const bool do_unroll = ( config.M / (by*config.Mnext) >= unroll_reduce );
+    KERNEL_UNROLL_SWITCH(do_unroll, unroll_reduce, UNROLL, {
+      reduce_kernel<Reducer, ndim, AType, DType, OType, OP, UNROLL>
+      <<< config.kernel_1.gridDim, config.kernel_1.blockDim, config.kernel_1.shMemSize, stream>>>(
+        config.N, config.M, addto, big.dptr<DType>(), small_dptr, big.shape_.get<ndim>(),
+        small.shape_.get<ndim>(), config.rshape.get<ndim>(), config.rstride.get<ndim>(),
+        config.Mnext, config.kernel_1.do_transpose);
+    });
+    MSHADOW_CUDA_POST_KERNEL_CHECK(reduce_kernel);
+
+    if (config.Mnext > 1) {
+      reduce_lines_kernel<Reducer, OType>
+      <<< config.kernel_2.gridSize, config.kernel_2.blockSize, 0, stream >>>
+        (config.N, config.Mnext, req == kAddTo, config.N, small_dptr, small.dptr<OType>());
+      MSHADOW_CUDA_POST_KERNEL_CHECK(reduce_lines_kernel);
+    }
+  }
+
+
+
+
+
+
+
+
+*/
+
+
 }
 
 
