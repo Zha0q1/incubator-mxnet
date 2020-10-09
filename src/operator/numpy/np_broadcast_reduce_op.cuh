20e923da7e01fa98fdc7eff2639fe8c2e60ee6f6
Zhu <zhaoqzhu@3c22fbbb4e1a.ant.amazon.com>
2020-10-09 14:09:04 -0700

    a

--- src/operator/numpy/np_broadcast_reduce_op.cuh
+++ src/operator/numpy/np_broadcast_reduce_op.cuh
@@ -111,7 +111,7 @@ void NumpyArgMinMaxReduce(Stream<gpu> *s, const TBlob& in_data, const TBlob& out
     const bool do_unroll = ( config.M / (by*config.Mnext) >= unroll_reduce );
     KERNEL_UNROLL_SWITCH(do_unroll, unroll_reduce, UNROLL, {
       std::cout << "boom boom u77777777" << std::endl;
-      reduce_kernel<Reducer, NDim, OType, DType, OType, mxnet::op::mshadow_op::myOp<DType, OType>, UNROLL>
+      reduce_kernel<Reducer, NDim, OType, DType, OType, mxnet::op::mshadow_op::myOp<DType, OType>, UNROLL, true>
       <<< config.kernel_1.gridDim, config.kernel_1.blockDim, config.kernel_1.shMemSize, stream>>>(
         config.N, config.M, addto, in_data.dptr<DType>(), out_dptr, in_data.shape_.get<NDim>(),
         out_data.shape_.get<NDim>(), config.rshape.get<NDim>(), config.rstride.get<NDim>(),
